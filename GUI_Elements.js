f7f63b6919e538ec4da947f328bfcbb2a750d5d1
ThusharaF <thushara.fernando@itelasoft.com>
2025-05-28 21:53:17 +0530

    GUI Elements page updated

--- GUI_Elements.js
+++ GUI_Elements.js
@@ -2,4 +2,20 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 
 (async function exampleTest() {
   let driver = await new Builder().forBrowser('chrome').build();
+
+  try
+  {
+    await driver.get("https://testautomationpractice.blogspot.com/");
+    // Wait for the page to load
+    await driver.wait(until.titleIs("Automation Testing Practice"), 10000);
+    console.log("Page title is correct");
+  }
+  catch(err)
+  {
+    console.error("Test failed:", err);
+  }
+  finally
+  {
+    await driver.quit();
+  }
 })();
